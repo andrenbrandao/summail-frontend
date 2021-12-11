20d4ac24eff69cf14ccaabdd20bd29c1000c11fe
André Brandão <andrenbrandao@gmail.com>
2021-12-11 20:31:01 -0300

    feat: create header

---
+++ components/layout.tsx
@@ -0,0 +1,11 @@
+import Header from './header'
+
+const Layout: React.FC = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+    </>)
+}
+
+export default Layout;
