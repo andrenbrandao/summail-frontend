5b34ef173b5bd3efd6e57712b5254cb46b278f37
André Brandão <andrenbrandao@gmail.com>
2023-10-01 23:03:24 -0300

    feat: add react-toastify to show alert

--- components/google-login-button.tsx
+++ components/google-login-button.tsx
@@ -4,12 +4,18 @@ import {
   GoogleLoginProps,
 } from '@react-oauth/google'
 import axios from 'axios'
+import { toast } from 'react-toastify'
 
 async function handleCredentialResponse(response: CredentialResponse) {
-  await axios.post('http://localhost:4000/local/login', response, {
-    timeout: 5000,
-    withCredentials: true,
-  })
+  try {
+    await axios.post('http://localhost:400/local/login', response, {
+      timeout: 5000,
+      withCredentials: true,
+    })
+  } catch (error) {
+    console.error(error)
+    toast.error('Login failed!')
+  }
 }
 
 export default function GoogleLoginButton({
