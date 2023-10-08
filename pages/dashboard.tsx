2317008707d29a6810dfb8a9086648d8bb806c30
André Brandão <andrenbrandao@gmail.com>
2023-10-07 22:30:48 -0300

    feat(wip): add cta to authorize gmail

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,14 +1,31 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
 import Head from 'next/head'
 import Image from 'next/image'
 import {
   UserCircleIcon,
   ArrowLeftOnRectangleIcon,
 } from '@heroicons/react/24/outline'
-import { Sidebar } from 'flowbite-react'
+import { Sidebar, Card, Button } from 'flowbite-react'
+import { useGoogleLogin } from '@react-oauth/google'
+import axios from 'axios'
 
 const Dashboard: NextPage = () => {
+  const googleLogin = useGoogleLogin({
+    flow: 'auth-code',
+    overrideScope: true,
+    scope:
+      'https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.modify',
+    onSuccess: async (codeResponse) => {
+      console.log(codeResponse)
+      const tokens = await axios.post('http://localhost:3001/auth/google', {
+        code: codeResponse.code,
+      })
+
+      console.log(tokens)
+    },
+    onError: (errorResponse) => console.log(errorResponse),
+  })
+
   return (
     <div className="bg-gray-50">
       <Head>
@@ -43,7 +60,18 @@ const Dashboard: NextPage = () => {
               </Sidebar.ItemGroup>
             </Sidebar.Items>
           </Sidebar>
-          <main className={styles.container}></main>
+          <main className="p-6">
+            <Card className="max-w-lg">
+              <h2 className="text-md font-bold">Grant Gmail Permissions</h2>
+              <p className="text-sm text-gray-500 leading-5">
+                Enable Summail to retrieve your newsletters and deliver them at
+                your convenience.
+              </p>
+              <Button onClick={() => googleLogin()} color="blue">
+                Authorize Gmail
+              </Button>
+            </Card>
+          </main>
         </div>
       </div>
     </div>
